7be8547ecf4c21431e13b367ff1652aa4278583a
rushikeshkamurthi <k.hrushikesh007@gmail.com>
2023-08-03 16:16:07 +0530

    setting up project

---
+++ src/main/java/marcos/Login/LoginFramwork/LoginFramworkApplication.java
@@ -0,0 +1,13 @@
+package marcos.Login.LoginFramwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginFramworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginFramworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marcos/Login/LoginFramwork/LoginFramworkApplicationTests.java
@@ -0,0 +1,13 @@
+package marcos.Login.LoginFramwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginFramworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
